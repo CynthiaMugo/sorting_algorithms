f0596f652ebd45ce9f50c11d2b2b31753ddcc9c8
CynthiaMugo <cynthiamugo25@gmail.com>
2022-11-07 23:08:15 -0800

    slttn bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -33,7 +33,7 @@ void bubble_sort(int *array, size_t size)
 		bubbly = true;
 		for (i = 0; i < len - 1; i++)
 		{
-			if (array[i] > array[]i + 1)
+			if (array[i] > array[i + 1])
 			{
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
